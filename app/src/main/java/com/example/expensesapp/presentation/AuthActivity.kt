698b5075cf32449bc5e49acfe8772ad8a633704a
ysur67 <rus.muratow@gmail.com>
2021-11-06 18:01:44 +0300

    ref: rename main activity to auth activity

--- app/src/main/java/com/example/expensesapp/presentation/AuthActivity.kt
+++ app/src/main/java/com/example/expensesapp/presentation/AuthActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.expensesapp.R
-import com.example.expensesapp.databinding.ActivityMainBinding
+import com.example.expensesapp.databinding.ActivityAuthBinding
 import com.example.expensesapp.domain.UserViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     val userViewModel: UserViewModel by viewModel()
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navController = findNavController(R.id.fragmentContainerView)
         binding.bottomNavigationView.setupWithNavController(navController)
+        userViewModel.isAuthenticated.observe(this, {
+            if (it != true) {
+                return@observe
+            }
+
+        })
     }
 }
